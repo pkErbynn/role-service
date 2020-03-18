2a1e3b3842042106cfff483cd7c81ea82cbfaa53
pkerbynn <john.erbynn@gmail.com>
2020-03-18 18:32:46 +0000

    [issue]: ...

--- Models/RoleContext.cs
+++ Models/RoleContext.cs
@@ -7,5 +7,7 @@ namespace io.turntabl.RoleService.Models
     {
         public RoleContext(DbContextOptions<RoleContext> options) : base(options) { }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<Role> Role { get; set; }
+
     }
 }
