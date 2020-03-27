d2d0c5ef8c5dba6f3a36d1e7244e62e9d312a5d6
pkerbynn <john.erbynn@gmail.com>
2020-03-27 09:27:42 +0000

    doc: commented one relatonship out

--- Models/RoleServiceContext.cs
+++ Models/RoleServiceContext.cs
@@ -11,14 +11,34 @@ namespace io.turntabl.RoleService.Models
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(builder);
             
-            builder.Entity<Employee>().Property(employee => employee.role).IsRequired();
+            builder.Entity<Role>().ToTable("roles");
+            builder.Entity<Role>().HasKey(p => p.role_id);
+            builder.Entity<Role>().Property(p => p.role_id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Role>().Property(p => p.role_name).IsRequired();
+            builder.Entity<Role>().Property(p => p.role_description).IsRequired();
             
+            // table relationship
             builder.Entity<Role>()
                 .HasMany(p => p.employees)
                 .WithOne(p => p.role)
                 .HasForeignKey(p => p.role_id);
+            
+            builder.Entity<Employee>().ToTable("employees");
+            builder.Entity<Employee>().HasKey(p => p.employee_id);
+            builder.Entity<Employee>().Property(p => p.employee_id).IsRequired().ValueGeneratedOnAdd();
+            builder.Entity<Employee>().Property(p => p.employee_fname).IsRequired();
+            builder.Entity<Employee>().Property(p => p.employee_lname).IsRequired();
+            builder.Entity<Employee>().Property(p => p.employee_email).IsRequired();
+            builder.Entity<Employee>().Property(p => p.employee_address).IsRequired();
+            
+            // builder.Entity<Employee>()
+            //     .HasOne(p => p.role)
+            //     .WithMany(p => p.employees)
+            //     .HasForeignKey(p => p.role_id);
+            
+            base.OnModelCreating(builder);
+
         }
     }
 }
